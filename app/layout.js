9283bc391791673aa5b944e7202a18d2df85428c
Rishiraj Sharma <rishiraj.sharma@successtive.tech>
2026-05-22 14:22:10 +0530

    Task Id-7 chatbot UI

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en" className="h-full">
+            <body className="h-full overflow-hidden">{children}</body>
         </html>
     )
 }
